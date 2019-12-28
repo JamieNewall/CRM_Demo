0f465353f29e499fb7b28fd0f120093920bc09f1
Jamie Newall <jnewall93@outlook.com>
2019-12-28 13:38:39 +0000

    All loaders installed and Build Working

--- src/about.js
+++ src/about.js
@@ -4,4 +4,9 @@ import img from './assets/img/Praetura-Ventures-logo_white_rgb_small1.png';
 
 console.log('hello');
 
+
 document.querySelector('#img').src = img;
+
+async function es6Function() {
+    console.log('hello world');
+}
